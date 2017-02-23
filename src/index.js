9fc9227010189b3642364ce96bf219dab2801028
Nick <nicknchambers@gmail.com>
2017-02-23 12:56:00 -0700

    posts a grudge to /api/grudges with server returning app.locals.grudges

--- src/index.js
+++ src/index.js
@@ -1,11 +1,44 @@
-$(document).ready(function(){
+class Grudge {
+  constructor(name, offense, date) {
+    this.id = Date.now();
+    this.name = name;
+    this.offense = offense;
+    this.forgiven = false;
+    this.date = date;
+  }
+}
+
+var grudge = new Grudge()
+
+//listeners
+$(document).ready(() => {
   getGrudges()
 });
 
+$('.grudge-submit').click('click', (e) => {
+  e.preventDefault()
+  const newGrudge = createGrudge()
+  postGrudge(newGrudge)
+})
+
+//functions
 function getGrudges() {
-  $.get('/grudges', function(grudges) {
+  $.get('/api/grudges', (grudges) => {
     appendGrudgeList(grudges)
   })
+}
+
+function postGrudge(grudge) {
+  $.post('/api/grudges', grudge, (grudges) => {
+    appendGrudgeList(grudges)
+  })
+}
+
+function createGrudge() {
+  let name = $('.grudge-name').val()
+  let description = $('.grudge-description').val()
+  let date = $('.grudge-date').val()
+  return new Grudge(name, description, date)
 }
 
 function appendGrudgeList(grudges) {
